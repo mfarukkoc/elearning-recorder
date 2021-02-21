76e788fbb15522a06fe4cc39014af3d7a1254cd2
grygry12345 <41829095+grygry12345@users.noreply.github.com>
2021-02-21 13:07:10 +0300

    Deneme

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use(
   })
 );
 
+// Video POST handler.
+app.post("/video", function (req, res) {
+  upload_video(req, function(err, data) {
+
+    if (err) {
+      return res.status(404).end(JSON.stringify(err));
+    }
+
+    res.send(data);
+  });
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
